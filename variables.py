dbef36b2590bb7d03198f3e1cc1b30dcf56a0418
DPK252000 <113545795+DPK252000@users.noreply.github.com>
2022-09-21 19:06:10 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+print(6.9)
+
+a=3
+b=6
+c=9
+print(b)
+b=9
+print(b)
+
+a=9
+b=10
+c=13
+print(c)
+
+a=10 ; b=13 ; c=16
+print(a,b)
+print(a,b,b)
+print(d)
+
+a,b,c,d=12,14,16,18
+a,b,c,d=12,13,19
+a,b=2,4,6,9
+
+a=7.9
+print(type(a))
+
